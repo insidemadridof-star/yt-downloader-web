985e0b91ee0e326f78081caa99cac7a49d1fa192
insidemadridof-star <insidemadridof@gmail.com>
2026-03-23 11:57:51 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,37 +1,3 @@
-from flask import Flask, request, send_file, render_template
-import yt_dlp
-import os
-import uuid
-
-app = Flask(__name__)
-
 @app.route('/')
 def home():
-    return render_template('index.html')
-
-@app.route('/download', methods=['POST'])
-def download():
-    url = request.form.get('url')
-
-    if not url:
-        return "Error: No URL provided", 400
-
-    filename = f"{uuid.uuid4()}.mp4"
-
-    ydl_opts = {
-        'outtmpl': filename,
-        'format': 'mp4'
-    }
-
-    try:
-        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-            ydl.download([url])
-
-        return send_file(filename, as_attachment=True)
-
-    except Exception as e:
-        return f"Error descargando el vídeo: {str(e)}", 500
-
-    finally:
-        if os.path.exists(filename):
-            os.remove(filename)
+    return "SI VES ESTO, EL PROBLEMA ES EL HTML"
